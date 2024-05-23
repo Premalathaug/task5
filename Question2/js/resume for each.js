b877182b95da311aba8a2384dce7f3b4bc8bdc8e
premalathaug <premalatha21292@gmail.com>
2024-05-23 21:06:34 +0530

    firstcommit

---
+++ Question2/js/resume for each.js
@@ -0,0 +1,16 @@
+var obj = {
+    name: "Premalatha UG",
+    DateofBirth:"21May1992",
+    Address:"234,TiruvalluvarStreet,Tambaram,Chennai-600089",
+    EducationQualification:"M.sc(cs)",
+    Institution:"Abcd College",
+    Experience:"Nil",
+    Email:"premalatha21292@gmail.com",
+    PhoneNo:"9876543210",
+    Skills:"c++,java",
+    Hobbies:`Reading Books,Playing ThrowBall`
+    };
+    Object.keys(obj).forEach(key => {
+        console.log(key,obj[key]);
+        
+    });
---
+++ Question2/js/resume for of.js
@@ -0,0 +1,15 @@
+var obj = {
+    name: "Premalatha UG",
+    DateofBirth:"21May1992",
+    Address:`234,TiruvalluvarStreet,Tambaram,Chennai,600089`,
+    EducationQualification:"M.sc(cs)",
+    Institution:"Abcd College",
+    Experience:"Nil",
+    Email:"premalatha21292@gmail.com",
+    PhoneNo:"9876543210",
+    Skills:"c++,java",
+    Hobbies:`Reading Books,Playing ThrowBall`,
+    };
+    for (const[key,value]of Object.entries(obj)) {
+        console.log(key,value);
+      }
